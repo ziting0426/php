9ca2cebb67ccc34d23d6b7e1deeed720596d3fe0
ziting0426 <a1113306@mail.nuk.edu.tw>
2024-03-04 15:27:42 +0800

    Initial commit

--- week1/week1.php
+++ week1/week1.php
@@ -1,9 +1,17 @@
-<html>
-<?php
-    echo "hello world!";
-    echo "<br>";
-    echo "A1113306";
-    echo "<br>";
-    echo "week1 hw"
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo "hello world!";
+        echo "<br>";
+        echo "A1113306";
+        echo "<br>";
+        echo "week1 hw"
+    ?>
+</body>
 </html>
